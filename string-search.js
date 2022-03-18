ecaaa4a522e4570413a459ec91785a812e4501cf
unknown <mahmodulshahin665@gmail.com>
2022-03-18 21:42:20 +0600

    add more file

---
+++ string-search.js
@@ -0,0 +1,10 @@
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 13pro max',
+    'yellow laptop with black camera',
+    '1X59 Lenovo commercial yoga laptop',
+    'LG supernova laptop',
+    'HTC low price phone',
+    'purple color phone with laptop'
+];
+const searching = 'laptop';
