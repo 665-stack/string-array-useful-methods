126ccf63990398468962d7722dcca1aa8c9b9c21
unknown <mahmodulshahin665@gmail.com>
2022-03-18 21:03:53 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+const userInput = 'maHmoDulShahin';
+const savedUserName = 'mahmodulshahin';
+console.log(userInput.toLowerCase())
+//akhaner toLowerCase() deyar por user jotoi uppercase/lowercase kore userInput dak na keno, shob lowercase kore dibe;
+if (userInput.toLowerCase() == savedUserName) {
+    console.log('user exist')
+}
