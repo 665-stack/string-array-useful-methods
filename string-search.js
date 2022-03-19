012b347d731e704537e80dd4e6adf43280fe0286
unknown <mahmodulshahin665@gmail.com>
2022-03-19 10:08:50 +0600

    add .includes, .startsWith

--- string-search.js
+++ string-search.js
@@ -4,7 +4,40 @@ const products = [
     'yellow laptop with black camera',
     '1X59 Lenovo commercial yoga laptop',
     'LG supernova laptop',
-    'HTC low price phone',
-    'purple color phone with laptop'
+    'HTC low price Phone',
+    'purple color phone with Laptop'
 ];
 const searching = 'laptop';
+const output = [];
+
+//.indexof
+for (const product of products) {
+    if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+        // output.push(product);
+    }
+}
+//.includes;; ei .includes tao onekta .indexof er motoi kaj kore;
+for (const product of products) {
+    if (product.toLowerCase().includes(searching.toLowerCase())) {
+        // output.push(product);
+    }
+}
+//another example for .starsWith()
+const products2 = [
+    'Dell hardcore i9 12gb laptop',
+    'Iphone 13pro max',
+    'Dell thempad',
+    'Google pixel 6',
+    'Dell commercial yoga laptop',
+    'Samsung galaxy fold 3',
+    'laptop dell'
+];
+const searching2 = 'dell';
+const output2 = []
+//.startsWith() diye buja jay je, ami jei jinish ta khustaci sheta string er shurute ace naki nai;
+for (const product2 of products2) {
+    if (product2.toLowerCase().startsWith(searching2.toLowerCase())) {
+        output2.push(product2);
+    }
+}
+console.log(output2)
