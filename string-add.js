e7b8e80c43399f7a1bc4cf93127854c587230a15
unknown <mahmodulshahin665@gmail.com>
2022-03-19 15:33:29 +0600

    add concat and join

---
+++ string-add.js
@@ -0,0 +1,13 @@
+//======== .concat() =========
+const first = 'My';
+const second = ' City';
+// const fullString = first + second;
+//ababe caile onk bar concat kore add korte parbo;
+const fullString = first.concat(second).concat(' is good.')
+console.log(fullString);
+//======== .join() ==============
+const words = ['alim', 'badhon', 'cemeron', 'david'];
+// const allwords = words.join('')
+// const allwords = words.join(' ')
+const allwords = words.join(', ')
+console.log('allwords -- ', allwords)
--- string-slice.js
+++ string-slice.js
@@ -2,16 +2,20 @@
 const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
 //akhane anthem name er string er word gulo k divide kore akta Array banay felbe;
 const words = anthem.split(' ');
-console.log(words)
+console.log('words -- ', words)
 //akhane jekhane jekhane 'a' paice, shekhanei divide kore dice;
 const wihtoutA = anthem.split('a');
-console.log(wihtoutA)
+console.log('withoutA -- ', wihtoutA)
 
 //=========== slice ===========
 //akhane anthem name er string k index number diye slice kora hoice
 const smallSlice = anthem.slice(5, 13);
-console.log(smallSlice)
+console.log('smallSlice -- ', smallSlice)
 //=========== substr ============
-//akhaneo anthem name er string k index number er maddome slice kora hoice. but akhane slice korar system ta aktu alada.Jemon- akhane bola hoice 18 tomo index er por theke 3 index er modde ja ace tai dao; 
-const anotherPart = anthem.substr(10, 9);
-console.log(anotherPart)
+//akhaneo anthem name er string k index number er maddome slice kora hoice. but akhane slice korar system ta aktu alada.Jemon- akhane bola hoice 11 tomo index er por theke 8 index er modde ja ace tai dao; 
+const anotherPart = anthem.substr(11, 8);
+console.log('anotherPart -- ', anotherPart)
+//=========== substring ============
+//atao onkta .slice() er moto kaj kore;
+const againAnotherPart = anthem.substring(11, 15);
+console.log('againAnotherPart -- ', againAnotherPart);
