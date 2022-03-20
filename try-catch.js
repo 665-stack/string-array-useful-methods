90c16e9c0b4e2f65caff8362ae663d07f9d87647
unknown <mahmodulshahin665@gmail.com>
2022-03-20 19:06:46 +0600

    add try-catch

---
+++ try-catch.js
@@ -0,0 +1,20 @@
+const myName = 'ami';
+const student = undefined;
+// console.log(student.name);
+
+//akhaner try catch jinish hocca je amon, jodi amar mone hoy j ei code tar fole error diye pura programe take nosto kore dite pare tokhon shei code try er bitore rakbo; and jodi ki error dicca sheta dekte cai taile sheta catch er bitore likbo. jekhane error and onnano code er result o thakbe;
+try {
+    myName = 'tumi';
+
+}
+catch (error) {
+    console.log('get error -- ', error)
+}
+console.log(10)
+console.log(myName)
+
+// for (let i = 0; i < 10 i++) {
+
+// }
+// name = 'tumi';
+// console.log(name)
